55b47c8811f033c286d4d62aba17e8150b4e2431
SAMAKSH MITTAL <sam.mittal44@gmail.com>
2021-07-01 12:56:25 +0530

    intial setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,18 @@
+import { BrowserRouter, Link, Switch, Route } from "react-router-dom";
+import Summary from "./pages/summary";
+import Detail from "./pages/detail";
+import Header from "./comp/header";
 import "./App.css";
 
 function App() {
   return (
     <>
-      <div></div>
+      <div>
+        <BrowserRouter>
+          <Header />
+          <Route exact path="/" component={Summary} />
+        </BrowserRouter>
+      </div>
     </>
   );
 }
---
+++ client/src/comp/header.js
@@ -0,0 +1,11 @@
+const Header = () => {
+  return (
+    <>
+      <div>
+        <h1 className="text-center text-warning mt-3 mb-4">COINS</h1>
+      </div>
+    </>
+  );
+};
+
+export default Header;
